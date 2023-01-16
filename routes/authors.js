56d83688721a3dea58f57951cc71399185363751
Faraz <farazgojali@gmail.com>
2023-01-16 20:48:36 +0500

    finished authors route

--- routes/authors.js
+++ routes/authors.js
@@ -2,6 +2,7 @@ const express = require('express');
 const author = require('../models/author');
 const router = express.Router();
 const Author = require('../models/author');
+const Book = require('../models/book');
 
 //all authors
 router.get('/', async (req, res) => {
@@ -25,6 +26,20 @@ router.get('/new', (req, res) => {
     res.render('authors/new', { author: new Author() });
 })
 
+//get author by id
+router.get('/:id', async (req, res) => {
+    try {
+        const author = await Author.findById(req.params.id);
+        const books = await Book.find({ author: author.id }).limit(6).exec();
+        res.render('authors/show', {
+            author: author,
+            booksByAuthor: books
+        })
+    } catch {
+        res.redirect("/")
+    }
+});
+
 //create author route
 router.post('/', async (req, res) => {
     const author = new Author({
@@ -32,8 +47,8 @@ router.post('/', async (req, res) => {
     })
     try {
         const newAuthor = await author.save();
-        // res.redirect(`authors/${newAuthor.id}`)
-        res.redirect('authors')
+        res.redirect(`authors/${newAuthor.id}`)
+        // res.redirect('authors')
     } catch {
         res.render('authors/new', {
             author: author,
@@ -42,5 +57,50 @@ router.post('/', async (req, res) => {
     }
 
 });
+
+router.get('/:id/edit', async (req, res) => {
+    try {
+        const author = await Author.findById(req.params.id);
+        res.render('authors/edit', { author: author });
+    } catch {
+        res.redirect('/authors')
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    let author;
+    try {
+        author = await Author.findById(req.params.id);
+        author.name = req.body.name;
+        await author.save();
+        res.redirect(`/authors/${author.id}`);
+    } catch {
+        if (author == null) {
+            res.redirect('/');
+        } else {
+            res.render('authors/edit', {
+                author: author,
+                errorMessage: 'Error updating Author'
+            })
+        }
+    }
+
+})
+
+router.delete('/:id', async (req, res) => {
+    let author;
+    try {
+        author = await Author.findById(req.params.id);
+        await author.remove();
+        res.redirect('/authors');
+    } catch {
+        if (author == null) {
+            res.redirect('/');
+        } else {
+            res.redirect(`/authors/${author.id}`);
+        }
+    }
+})
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 
 const indexRouter = require('./routes/index');
 const authorRouter = require('./routes/authors');
@@ -14,6 +15,7 @@ app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
+app.use(methodOverride('_method'));
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
